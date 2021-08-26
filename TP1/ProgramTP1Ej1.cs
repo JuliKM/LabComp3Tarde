ead85d922ff80ce10c47fe17824781cec80b6831
JuliKM <57647619+JuliKM@users.noreply.github.com>
2021-08-26 06:57:24 -0300

    Add files via upload

---
+++ TP1/ProgramTP1Ej1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace TpUno
+{
+    class Program1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Con for");
+            for (int i = 1; i < 101; i++)
+            {
+                if (i%2==0 && i % 5 == 0) Console.Write(i + " ");
+            }
+            Console.WriteLine("\nCon while");
+            int j = 1;
+            while (j < 101) {
+                if (j % 2 == 0 && j % 5 == 00) Console.Write(j + " ");
+                j++;
+            }
+            Console.WriteLine("\nCon do/while");
+            int k = 1;
+            do
+            {
+                if (k % 2 == 0 && k % 5 == 00) Console.Write(k + " ");
+                k++;
+            } while (k < 101);
+        }
+        
+    }
+}
